5010e7dbee947b9b7073c69fff6781fc95f0295a
Nur <nuriswanto66@gmail.com>
2023-03-23 15:41:23 +0700

    Dasar pemrograman kotlin

--- src/main/kotlin/When.kt
+++ src/main/kotlin/When.kt
@@ -1,6 +1,6 @@
 fun main() {
 
-    val nilai : String = "D"
+    val nilai : String = "A"
 
     when (nilai) {
         "A" -> {
@@ -18,5 +18,38 @@ fun main() {
         else -> {
             println("Try again next year")
         }
+    }
+
+    // When multi option
+    when(nilai){
+        "A", "B", "C" -> {
+            println("Congratulation, you are pass the examp")
+        } else -> {
+            println("Try Again next year")
+        }
+    }
+
+    // When expression In
+    val nilaiLulus : Array<String> = arrayOf("A", "B", "C")
+    when (nilai) {
+        in nilaiLulus -> println("You are graduation")
+        !in nilaiLulus -> println("Try again next year")
+    }
+
+
+    // When expression Is
+    val name : String = "Nur"
+    when (name) {
+        is String -> println("Name is string")
+        !is String -> println("Name is not string")
+    }
+
+    // When pengganti if else
+    val nilaiUjian = 90
+    when{
+        nilaiUjian > 90 -> println("Amazing")
+        nilaiUjian > 80 -> println("Good")
+        nilaiUjian > 70 -> println("Not bad")
+        else -> println("Bad")
     }
 }
