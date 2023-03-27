1ea4b151c3d325c44081baf38b41c2f1ec2ca28f
Nur <nuriswanto66@gmail.com>
2023-03-27 10:27:18 +0700

    Properties Overridng

---
+++ src/main/kotlin/oop/app/PropertiesOverridaing.kt
@@ -0,0 +1,13 @@
+package oop.app
+
+import oop.data.PropertiesOverriding
+import oop.data.childProrson
+
+fun main(){
+    val number = PropertiesOverriding()
+    val person = childProrson()
+
+    println(number.corner)
+    println()
+    println(person.corner)
+}
